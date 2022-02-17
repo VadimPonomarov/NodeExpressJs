14c1a2b422604eae28ed7b37fe1a2acf40d437c1
VadimPonomarov <pvs.versia@gmail.com>
2022-02-17 21:55:58 +0200

    Lesson_2_HW

--- server/App.js
+++ server/App.js
@@ -1,16 +1,56 @@
 const app = () => {
     const express = require('express')
     const path = require('path')
+    const fs = require('fs')
     const hbs = require('hbs')
+    const userList = fs.readFileSync(
+        path.join(process.cwd(),
+            'server/db/users.txt'
+        ),
+        {encoding: 'utf8'}
+    )
 
     const app = express()
     const port = 5000
 
     app.set('views', path.join(__dirname, 'static'))
     app.set('view engine', 'hbs')
+    app.use(express.urlencoded());
+    app.use(express.json());
 
     app.get('/', (req, res) => {
         res.render('Login')
+
+    })
+    app.get('/user_list', (req, res) => {
+        const data = []
+        userList.trim().split('\n').forEach(item => data.push(JSON.parse(item)))
+        res.render('UserList', {data})
+    })
+
+    app.get('/user_list/:email', (req, res) => {
+        const data = userList.trim().split('\n')
+            .filter(item => JSON.parse(item).email === req.params.email)
+        res.render('UserDetails', {data: JSON.parse(data[0])})
+    })
+
+    app.post('/home', (req, res) => {
+        const data = userList.trim().split('\n')
+            .find(item => JSON.parse(item).email === req.body.email)
+        if (data) {
+            return res.redirect('error_email')
+        }
+        fs.appendFile(path.join(process.cwd(), 'server/db/users.txt'),
+            `${JSON.stringify(req.body)}\n`, () => {
+                res.redirect('/user_list')
+            })
+    })
+    app.get('/error_email', (req, res) => {
+        res.render('ErrorEmail')
+    })
+
+    app.use((req, res) => {
+        res.render('Error404')
     })
 
     app.listen(port, () => {
