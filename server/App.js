897d63f52ccbbd2a1dcbf33dc20fe41df6e9157a
VadimPonomarov <pvs.versia@gmail.com>
2022-02-17 15:42:00 +0200

    Form

--- server/App.js
+++ server/App.js
@@ -1,11 +1,16 @@
 const app = () => {
     const express = require('express')
+    const path = require('path')
+    const hbs = require('hbs')
+
     const app = express()
     const port = 5000
 
+    app.set('views', path.join(__dirname, 'static'))
+    app.set('view engine', 'hbs')
 
     app.get('/', (req, res) => {
-        res.send('Hello World!')
+        res.render('Login')
     })
 
     app.listen(port, () => {
