71d88a78bdd8d557bd6d59b02b0b22c87c544ef8
VadimPonomarov <pvs.versia@gmail.com>
2022-02-19 20:32:38 +0200

    Lesson_2_HW with response to feedback

--- server/App.js
+++ server/App.js
@@ -1,14 +1,10 @@
+const fs = require('fs');
+const path = require('path');
 const app = () => {
     const express = require('express')
     const path = require('path')
     const fs = require('fs')
     const hbs = require('hbs')
-    const userList = fs.readFileSync(
-        path.join(process.cwd(),
-            'server/db/users.txt'
-        ),
-        {encoding: 'utf8'}
-    )
 
     const app = express()
     const port = 5000
@@ -24,17 +20,35 @@ const app = () => {
     })
     app.get('/user_list', (req, res) => {
         const data = []
+        const userList = fs.readFileSync(
+            path.join(process.cwd(),
+                'server/db/users.txt'
+            ),
+            {encoding: 'utf8'}
+        )
         userList.trim().split('\n').forEach(item => data.push(JSON.parse(item)))
         res.render('UserList', {data})
     })
 
     app.get('/user_list/:email', (req, res) => {
+        const userList = fs.readFileSync(
+            path.join(process.cwd(),
+                'server/db/users.txt'
+            ),
+            {encoding: 'utf8'}
+        )
         const data = userList.trim().split('\n')
             .filter(item => JSON.parse(item).email === req.params.email)
         res.render('UserDetails', {data: JSON.parse(data[0])})
     })
 
     app.post('/home', (req, res) => {
+        const userList = fs.readFileSync(
+            path.join(process.cwd(),
+                'server/db/users.txt'
+            ),
+            {encoding: 'utf8'}
+        )
         const data = userList.trim().split('\n')
             .find(item => JSON.parse(item).email === req.body.email)
         if (data) {
